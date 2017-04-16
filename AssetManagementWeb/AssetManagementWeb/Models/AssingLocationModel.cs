1afa0abbda6e392ec6c4883ecd7ab087ef5254c9
Juha Pusa <juha.m.pusa@hotmail.com>
2017-04-16 14:09:16 +0300

    Lisätty TypeScripti projektiin ja alitettu Json sivun testi

---
+++ AssetManagementWeb/AssetManagementWeb/Models/AssingLocationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetManagementWeb.Models
+{
+    public class AssingLocationModel
+    {
+        public string AssetCode { get; set; }
+        public string LocationCode { get; set; }
+    }
+}
